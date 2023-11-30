487bc21d5ab68408282bd15d45eab468abf6d3d2
Aleksunder Volkov <t.me/aleksundervolkov>
2023-11-30 16:36:52 +0300

    Configure for jitpack

--- kukushka-sdk/build.gradle.kts
+++ kukushka-sdk/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    `maven-publish`
 }
 
 android {
@@ -36,8 +37,20 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.kukushka.sdk"
+                artifactId = "android-sdk"
+                version = "1.0.0"
+            }
+        }
+    }
 }
